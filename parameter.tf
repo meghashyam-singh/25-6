6688812db831f4114a75a9a1fd8d8ab1c133154f
megha <megha@gmail.com>
2026-06-26 00:42:26 +0530

    25-6

--- parameter.tf
+++ parameter.tf
@@ -21,3 +21,10 @@ resource "aws_ssm_parameter" "database_subnet_ids" {
     type = "StringList"
     value = join(",", aws_subnet.database[*].id)
 }
+
+resource "aws_ssm_parameter" "sg_ids" {
+    count = length(var.sg_names)
+    name = "${local.common_name}_${var.sg_names[count.index]}_sg_id"
+    type = "String"
+    value = aws_security_group.roboshop_sg[count.index].id
+}
